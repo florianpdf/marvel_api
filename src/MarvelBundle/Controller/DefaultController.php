5569c983ae34faab322cddd9340c2b7e81324c9e
Florian Grandjean <florian.pdf@gmail.com>
2016-07-21 15:06:22 +0200

    fin

--- src/MarvelBundle/Controller/DefaultController.php
+++ src/MarvelBundle/Controller/DefaultController.php
@@ -3,21 +3,22 @@
 namespace MarvelBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $characters = $this->container->get('marvel.api')->indexAction();
+        $characters = $this->container->get('marvel.api')->indexAction(2);
 
         return $this->render('MarvelBundle:Default:index.html.twig', array(
             'marvels' => $characters,
         ));
     }
 
-    public function showMarvelAction()
+    public function showMarvelAction(Request $request)
     {
-        $characters = $this->container->get('marvel.api')->showAction();
+        $characters = $this->container->get('marvel.api')->indexAction(1);
 
         return $this->render('MarvelBundle:Default:show.html.twig', array(
             'marvels' => $characters,
--- src/MarvelBundle/MarvelApi/MarvelCall.php
+++ src/MarvelBundle/MarvelApi/MarvelCall.php
@@ -2,8 +2,6 @@
 
 namespace MarvelBundle\MarvelApi;
 
-
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class MarvelCall
@@ -14,27 +12,27 @@ class MarvelCall
         $this->requestStack = $requestStack;
     }
 
-    public function indexAction()
+    public function indexAction($limit)
     {
         $api_key = '8b4b11c43611e66fda1b504f8cae9d7e';
         $md5 = md5('13d94c0c5ded9e1159568e675829465c37f97eece8b4b11c43611e66fda1b504f8cae9d7e');
 
-        $characters = json_decode(file_get_contents('https://gateway.marvel.com:443/v1/public/characters?limit=100&apikey=' . $api_key . '&ts=1&hash=' . $md5), true);
+        $id_marvel = $this->idMarvelAction();
+
+        $characters = json_decode(file_get_contents('https://gateway.marvel.com:443/v1/public/characters' . $id_marvel . '?limit=' . $limit . '&apikey=' . $api_key . '&ts=1&hash=' . $md5), true);
 
         return $characters;
     }
 
-    public function showAction(){
-        $api_key = '8b4b11c43611e66fda1b504f8cae9d7e';
-        $md5 = md5('13d94c0c5ded9e1159568e675829465c37f97eece8b4b11c43611e66fda1b504f8cae9d7e');
-
+    public function idMarvelAction()
+    {
         $request = $this->requestStack->getCurrentRequest();
 
-        $id_marvel = $request->get('marvel');
+        if ($request->get('marvel'))
+            $id_marvel = '/' . $request->get('marvel');
+        else
+            $id_marvel = '';
 
-        $characters = json_decode(file_get_contents('https://gateway.marvel.com:443/v1/public/characters/' . $id_marvel . '?limit=100&apikey=' . $api_key . '&ts=1&hash=' . $md5), true);
-
-        return $characters;
+        return $id_marvel;
     }
-
 }
